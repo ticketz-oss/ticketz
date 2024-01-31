bf63a89e62d958c4953bee558fb5cf737f7d55b3
Claudemir Todo Bom <claudemir@todobom.com>
2024-01-31 17:28:48 -0300

    backend: store seed status

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -12,5 +12,6 @@ module.exports = {
   database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
-  logging: process.env.DB_DEBUG === "true"
+  logging: process.env.DB_DEBUG === "true",
+  seederStorage: "sequelize",
 };
