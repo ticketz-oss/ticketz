80b7edb356ba765175b0f1a6392b07e552632a67
Bruno Cardoso <brunocgc@nvngroup.com.br>
2024-03-25 09:32:45 -0300

    Middleware para rotas de superadmin @brunocgc

--- backend/src/routes/helpRoutes.ts
+++ backend/src/routes/helpRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import isAuth from "../middleware/isAuth";
+import isSuper from "../middleware/isSuper";
 
 import * as HelpController from "../controllers/HelpController";
 
@@ -11,10 +12,10 @@ routes.get("/helps", isAuth, HelpController.index);
 
 routes.get("/helps/:id", isAuth, HelpController.show);
 
-routes.post("/helps", isAuth, HelpController.store);
+routes.post("/helps", isAuth, isSuper, HelpController.store);
 
-routes.put("/helps/:id", isAuth, HelpController.update);
+routes.put("/helps/:id", isAuth, isSuper, HelpController.update);
 
-routes.delete("/helps/:id", isAuth, HelpController.remove);
+routes.delete("/helps/:id", isAuth, isSuper, HelpController.remove);
 
 export default routes;
--- backend/src/routes/planRoutes.ts
+++ backend/src/routes/planRoutes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import isAuth from "../middleware/isAuth";
+import isSuper from "../middleware/isSuper";
 
 import * as PlanController from "../controllers/PlanController";
 
@@ -13,10 +14,10 @@ planRoutes.get("/plans/all", PlanController.list);
 
 planRoutes.get("/plans/:id", isAuth, PlanController.show);
 
-planRoutes.post("/plans", isAuth, PlanController.store);
+planRoutes.post("/plans", isAuth, isSuper, PlanController.store);
 
-planRoutes.put("/plans/:id", isAuth, PlanController.update);
+planRoutes.put("/plans/:id", isAuth, isSuper, PlanController.update);
 
-planRoutes.delete("/plans/:id", isAuth, PlanController.remove);
+planRoutes.delete("/plans/:id", isAuth, isSuper, PlanController.remove);
 
 export default planRoutes;
