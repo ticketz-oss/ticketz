dcf7dd08e2384c82329b90315a1dbb4f9cbfc55d
Claudemir Todo Bom <claudemir@todobom.com>
2024-05-06 21:25:14 -0300

    Enhance security on Invoices endpoints

--- backend/src/routes/invoicesRoutes.ts
+++ backend/src/routes/invoicesRoutes.ts
@@ -6,7 +6,7 @@ import * as InvoicesController from "../controllers/InvoicesController"
 const invoiceRoutes = express.Router();
 
 invoiceRoutes.get("/invoices", isAuth, InvoicesController.index);
-invoiceRoutes.get("/invoices/list", InvoicesController.list);
+invoiceRoutes.get("/invoices/list", isAuth, InvoicesController.list);
 invoiceRoutes.get("/invoices/all", isAuth, InvoicesController.list);
 invoiceRoutes.get("/invoices/:Invoiceid", isAuth, InvoicesController.show);
 invoiceRoutes.put("/invoices/:id", isAuth, InvoicesController.update);
