d92cd20652920015c41d2da051119666586b43e9
lufe70 <68402879+lufe70@users.noreply.github.com>
2024-05-18 01:22:10 -0300

    chore: Refactor PromptController to use req.user instead of decoding token

--- backend/src/controllers/PromptController.ts
+++ backend/src/controllers/PromptController.ts
@@ -25,20 +25,24 @@ type IndexQuery = {
 
 export const index = async (req: Request, res: Response): Promise<Response> => {
   const { pageNumber, searchParam } = req.query as IndexQuery;
-  const authHeader = req.headers.authorization;
-  const [, token] = authHeader.split(" ");
-  const decoded = verify(token, authConfig.secret);
-  const { companyId } = decoded as TokenPayload;
+
+  const { companyId } = req.user;
+
+  //const authHeader = req.headers.authorization;
+  //const [, token] = authHeader.split(" ");
+  //const decoded = verify(token, authConfig.secret);
+  //const { companyId } = decoded as TokenPayload;
   const { prompts, count, hasMore } = await ListPromptsService({ searchParam, pageNumber, companyId });
 
   return res.status(200).json({ prompts, count, hasMore });
 };
 
 export const store = async (req: Request, res: Response): Promise<Response> => {
-  const authHeader = req.headers.authorization;
-  const [, token] = authHeader.split(" ");
-  const decoded = verify(token, authConfig.secret);
-  const { companyId } = decoded as TokenPayload;
+  //const authHeader = req.headers.authorization;
+  //const [, token] = authHeader.split(" ");
+  //const decoded = verify(token, authConfig.secret);
+  //const { companyId } = decoded as TokenPayload;
+  const { companyId } = req.user;
   const { name, apiKey, prompt, maxTokens, temperature, promptTokens, completionTokens, totalTokens, queueId, maxMessages,voice,voiceKey,voiceRegion } = req.body;
   const promptTable = await CreatePromptService({ name, apiKey, prompt, maxTokens, temperature, promptTokens, completionTokens, totalTokens, queueId, maxMessages, companyId,voice,voiceKey,voiceRegion });
 
@@ -53,10 +57,11 @@ export const store = async (req: Request, res: Response): Promise<Response> => {
 
 export const show = async (req: Request, res: Response): Promise<Response> => {
   const { promptId } = req.params;
-  const authHeader = req.headers.authorization;
-  const [, token] = authHeader.split(" ");
-  const decoded = verify(token, authConfig.secret);
-  const { companyId } = decoded as TokenPayload;
+ // const authHeader = req.headers.authorization;
+ // const [, token] = authHeader.split(" ");
+ // const decoded = verify(token, authConfig.secret);
+ // const { companyId } = decoded as TokenPayload;
+ const { companyId } = req.user;
   const prompt = await ShowPromptService({ promptId, companyId });
 
   return res.status(200).json(prompt);
@@ -68,10 +73,11 @@ export const update = async (
 ): Promise<Response> => {
   const { promptId } = req.params;
   const promptData = req.body;
-  const authHeader = req.headers.authorization;
-  const [, token] = authHeader.split(" ");
-  const decoded = verify(token, authConfig.secret);
-  const { companyId } = decoded as TokenPayload;
+   // const authHeader = req.headers.authorization;
+ // const [, token] = authHeader.split(" ");
+ // const decoded = verify(token, authConfig.secret);
+ // const { companyId } = decoded as TokenPayload;
+ const { companyId } = req.user;
 
   const prompt = await UpdatePromptService({ promptData, promptId: promptId, companyId });
 
@@ -89,10 +95,11 @@ export const remove = async (
   res: Response
 ): Promise<Response> => {
   const { promptId } = req.params;
-  const authHeader = req.headers.authorization;
-  const [, token] = authHeader.split(" ");
-  const decoded = verify(token, authConfig.secret);
-  const { companyId } = decoded as TokenPayload;
+  // const authHeader = req.headers.authorization;
+ // const [, token] = authHeader.split(" ");
+ // const decoded = verify(token, authConfig.secret);
+ // const { companyId } = decoded as TokenPayload;
+ const { companyId } = req.user;
   try {
     const { count } = await Whatsapp.findAndCountAll({ where: { promptId: +promptId, companyId } });
 
