66637485f9396afd332edcd75f817fdded2de4b5
Claudemir Todo Bom <claudemir@todobom.com>
2024-06-13 10:36:23 -0300

    Allow company-wide quick messages - fix #28

--- backend/src/services/QuickMessageService/FindService.ts
+++ backend/src/services/QuickMessageService/FindService.ts
@@ -1,18 +1,31 @@
 import { Op } from "sequelize";
 import QuickMessage from "../../models/QuickMessage";
 import Company from "../../models/Company";
+import CheckSettings from "../../helpers/CheckSettings";
 
 type Params = {
   companyId: string;
   userId: string;
 };
 
+type QuickMessageWhere = {
+  companyId: string;
+  userId?: string;
+}
+
 const FindService = async ({ companyId, userId }: Params): Promise<QuickMessage[]> => {
+  const where: QuickMessageWhere = {
+    companyId,
+  } 
+
+  const quickMessagesSetting = await CheckSettings("quickMessages","individual"); 
+
+  if (quickMessagesSetting === "individual") {
+    where.userId = userId
+  }
+  
   const notes: QuickMessage[] = await QuickMessage.findAll({
-    where: {
-      companyId,
-      userId,
-    },
+    where,
     include: [{ model: Company, as: "company", attributes: ["id", "name"] }],
     order: [["shortcode", "ASC"]]
   });
