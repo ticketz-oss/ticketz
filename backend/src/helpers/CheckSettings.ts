72e838f03c8329602281594c7124a3852eb66047
Claudemir Todo Bom <claudemir@todobom.com>
2024-06-13 09:48:57 -0300

    CheckSettings: allow return default value

--- backend/src/helpers/CheckSettings.ts
+++ backend/src/helpers/CheckSettings.ts
@@ -1,7 +1,7 @@
 import Setting from "../models/Setting";
 import AppError from "../errors/AppError";
 
-const CheckSettings = async (key: string): Promise<string> => {
+const CheckSettings = async (key: string, defaultValue: string = null ): Promise<string> => {
   const setting = await Setting.findOne({
     where:
      {
@@ -10,11 +10,11 @@ const CheckSettings = async (key: string): Promise<string> => {
      }
   });
 
-  if (!setting) {
+  if (!setting && !defaultValue) {
     throw new AppError("ERR_NO_SETTING_FOUND", 404);
   }
 
-  return setting.value;
+  return setting?.value || defaultValue;
 };
 
 export default CheckSettings;
