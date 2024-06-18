809fb746e3c0f4651c17d304d581d47fc4787665
Claudemir Todo Bom <claudemir@todobom.com>
2024-06-18 08:31:28 -0300

    Correctly set the JRT cookie samesSite parameter - fix #67

--- backend/src/helpers/SendRefreshToken.ts
+++ backend/src/helpers/SendRefreshToken.ts
@@ -1,5 +1,12 @@
-import { Response } from "express";
+import { CookieOptions, Response } from "express";
 
 export const SendRefreshToken = (res: Response, token: string): void => {
-  res.cookie("jrt", token, { httpOnly: true });
+  const cookieOptions: CookieOptions = { httpOnly: true }; 
+
+  if (process.env.BACKEND_URL.startsWith("https:") ) {
+    cookieOptions.sameSite = "none";
+    cookieOptions.secure = true;
+  }
+   
+  res.cookie("jrt", token, cookieOptions);
 };
