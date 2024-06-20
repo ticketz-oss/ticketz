dc20c41595f22ab35e1bca49b1f2df7dd28fc097
Claudemir Todo Bom <claudemir@todobom.com>
2024-06-20 15:07:29 -0300

    New key storage on postgres

--- backend/src/helpers/authState.ts
+++ backend/src/helpers/authState.ts
@@ -5,68 +5,118 @@ import type {
 } from "@whiskeysockets/baileys";
 import { BufferJSON, initAuthCreds, proto } from "@whiskeysockets/baileys";
 import Whatsapp from "../models/Whatsapp";
-
-const KEY_MAP: { [T in keyof SignalDataTypeMap]: string } = {
-  "pre-key": "preKeys",
-  session: "sessions",
-  "sender-key": "senderKeys",
-  "app-state-sync-key": "appStateSyncKeys",
-  "app-state-sync-version": "appStateVersions",
-  "sender-key-memory": "senderKeyMemory"
-};
+import BaileysKeys from "../models/BaileysKeys";
+import { logger } from "../utils/logger";
 
 const authState = async (
   whatsapp: Whatsapp
 ): Promise<{ state: AuthenticationState; saveState: () => void }> => {
   let creds: AuthenticationCreds;
-  let keys: any = {};
+  const whatsappId = whatsapp.id;
+
+  const saveKey = async (type: string, key: string, value: any) => {
+    logger.debug({type, key, value}, "Storing key");
+    return BaileysKeys.upsert({ whatsappId, type, key, value: JSON.stringify(value)});
+  }
+  
+  const getKey = async (type: string, key: string) => {
+    const baileysKey = await BaileysKeys.findOne({
+      where: {
+        whatsappId,
+        type,
+        key,
+      }
+    });
+
+    logger.debug({type, key, baileysKey}, "Recovering key");
+
+    return baileysKey?.value ? JSON.parse(baileysKey.value) : null;
+  }
+  
+  const removeKey = async (type: string, key: string) => {
+    logger.debug({type, key}, "Deleting key");
+    return BaileysKeys.destroy({
+      where: {
+        whatsappId,
+        type,
+        key,
+      }
+    });
+  }
 
   const saveState = async () => {
     try {
       await whatsapp.update({
-        session: JSON.stringify({ creds, keys }, BufferJSON.replacer, 0)
+        session: JSON.stringify({ creds, keys: {} }, BufferJSON.replacer, 0)
       });
     } catch (error) {
       console.log(error);
     }
   };
 
-  // const getSessionDatabase = await whatsappById(whatsapp.id);
-
   if (whatsapp.session && whatsapp.session !== null) {
     const result = JSON.parse(whatsapp.session, BufferJSON.reviver);
     creds = result.creds;
-    keys = result.keys;
+    const {keys} = result;
+    
+    // conversion from old format (remove in the future)
+    if (Object.keys(keys).length) {
+      logger.debug({keys}, "Converting keys to new format");
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const type of Object.keys(keys)) {
+        logger.debug(`Converting keys of type ${type}`);
+        // eslint-disable-next-line no-restricted-syntax
+        for await (const key of Object.keys(keys[type])) {
+          await saveKey(type, key, keys[type][key]);
+        }
+      }
+      saveState();
+    }
+    
   } else {
     creds = initAuthCreds();
-    keys = {};
   }
 
   return {
     state: {
       creds,
       keys: {
-        get: (type, ids) => {
-          const key = KEY_MAP[type];
-          return ids.reduce((dict: any, id) => {
-            let value = keys[key]?.[id];
-            if (value) {
+        get: async (type, ids) => {
+          const data: { [_: string]: SignalDataTypeMap[typeof type] } = {};
+
+          // eslint-disable-next-line no-restricted-syntax
+          for (const id of ids) {
+            try {
+              // eslint-disable-next-line no-await-in-loop
+              let value = await getKey(type, id);
               if (type === "app-state-sync-key") {
                 value = proto.Message.AppStateSyncKeyData.fromObject(value);
               }
-              dict[id] = value;
+              data[id] = value;
+            } catch (error) {
+              logger.error(
+                `authState (69) -> error: ${error.message}`
+              );
+              logger.error(
+                `authState (72) -> stack: ${error.stack}`
+              );
             }
-            return dict;
-          }, {});
-        },
-        set: (data: any) => {
-          // eslint-disable-next-line no-restricted-syntax, guard-for-in
-          for (const i in data) {
-            const key = KEY_MAP[i as keyof SignalDataTypeMap];
-            keys[key] = keys[key] || {};
-            Object.assign(keys[key], data[i]);
           }
-          saveState();
+
+          return data;
+        },
+        set: async (data: any) => {
+          const tasks: Promise<unknown>[] = [];
+          // eslint-disable-next-line no-restricted-syntax, guard-for-in
+          for (const category in data) {
+            // eslint-disable-next-line no-restricted-syntax, guard-for-in
+            for (const id in data[category]) {
+              const value = data[category][id];
+              tasks.push(value ? saveKey(category, id, value) : removeKey(category, id));
+            }
+          }
+
+          await Promise.all(tasks);
         }
       }
     },
