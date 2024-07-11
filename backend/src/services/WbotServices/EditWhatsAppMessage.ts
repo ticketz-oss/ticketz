ticketz-oss/ticketz

commit eb9766f50ad7b31717b17f71c2134b1ac3ec515b

Apply mustaches on edited message

backend/src/services/WbotServices/EditWhatsAppMessage.ts
@@ -40,12 +40,13 @@ const EditWhatsAppMessage = async ({
   const wbot = await GetTicketWbot(ticket);
 
   const msg = JSON.parse(message.dataJson);
+  const formattedBody = formatBody(body, ticket.contact);
 
   try {
     await wbot.sendMessage(
       message.remoteJid,
       {
-        text: body,
+        text: formattedBody,
         edit: msg.key
       },
       {}
@@ -59,7 +60,7 @@ const EditWhatsAppMessage = async ({
 
     await OldMessage.upsert(oldMessage);
 
-    await message.update({ body, isEdited: true });
+    await message.update({ formattedBody, isEdited: true });
 
     const savedMessage = await Message.findOne({
       where: {

commit 2aff0adb8ee3f43bf1bf37af559a6d9ff42fbf4f

fix regression on message edit
caused by eb9766f50ad7b31717b17f71c2134b1ac3ec515b

backend/src/services/WbotServices/EditWhatsAppMessage.ts
@@ -60,7 +60,7 @@ const EditWhatsAppMessage = async ({
 
     await OldMessage.upsert(oldMessage);
 
-    await message.update({ formattedBody, isEdited: true });
+    await message.update({ body: formattedBody, isEdited: true });
 
     const savedMessage = await Message.findOne({
       where: {