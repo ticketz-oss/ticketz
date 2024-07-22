136a057940b5f76feec54298de00b7c4cbff4c82
Claudemir Todo Bom <claudemir@todobom.com>
2024-07-22 17:23:21 -0300

    make call unavailability message invisible

--- backend/src/services/WbotServices/wbotMonitor.ts
+++ backend/src/services/WbotServices/wbotMonitor.ts
@@ -46,7 +46,7 @@ const wbotMonitor = async (
 
         if (sendMsgCall.value === "disabled") {
           await wbot.sendMessage(node.attrs.from, {
-            text: "*Mensagem Automática:*\n\nAs chamadas de voz e vídeo estão desabilitas para esse WhatsApp, favor enviar uma mensagem de texto. Obrigado"
+            text: "\u200e*Mensagem Automática:*\n\nAs chamadas de voz e vídeo estão desabilitas para esse WhatsApp, favor enviar uma mensagem de texto. Obrigado"
           });
 
           const number = node.attrs.from.replace(/\D/g, "");
