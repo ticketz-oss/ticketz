7145d5395a5423820a7ae63227e4a0259cc3b13a
Claudemir Todo Bom <claudemir@todobom.com>
2024-07-22 17:14:42 -0300

    backend: inform contact that calls are not accepted - fix #122

--- backend/src/services/WbotServices/wbotMonitor.ts
+++ backend/src/services/WbotServices/wbotMonitor.ts
@@ -45,10 +45,9 @@ const wbotMonitor = async (
         });
 
         if (sendMsgCall.value === "disabled") {
-          // await wbot.sendMessage(node.attrs.from, {
-          //   text:
-          //     "*Mensagem Automática:*\n\nAs chamadas de voz e vídeo estão desabilitas para esse WhatsApp, favor enviar uma mensagem de texto. Obrigado",
-          // });
+          await wbot.sendMessage(node.attrs.from, {
+            text: "*Mensagem Automática:*\n\nAs chamadas de voz e vídeo estão desabilitas para esse WhatsApp, favor enviar uma mensagem de texto. Obrigado"
+          });
 
           const number = node.attrs.from.replace(/\D/g, "");
 
