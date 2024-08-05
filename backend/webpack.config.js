d26d186d45f49e47bf900d892b65fb0c1c19b4b7
Claudemir Todo Bom <claudemir@todobom.com>
2024-08-05 17:41:56 -0300

    config for webpack

---
+++ backend/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './dist/server.js', // Replace with your entry file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'production',
+  target: 'node', // Ensure this is set for node applications
+  externals: [
+    nodeExternals(),
+    function({ context, request }, callback) {
+      // Exclude `dist/models` directory for both relative and absolute paths
+      if (request.includes('./models/')) {
+        return callback(null, 'commonjs ' + request.replace(/^.*?(?=\.\/models\/)/, ''));
+      }
+      callback();
+    }
+  ],
+};
