d111a78142bdf25edb2c00a256c9c565f35298d6
Claudemir Todo Bom <claudemir@todobom.com>
2024-09-14 12:13:11 -0300

    Title component accepts className

--- frontend/src/components/Title/index.js
+++ frontend/src/components/Title/index.js
@@ -3,7 +3,7 @@ import Typography from "@material-ui/core/Typography";
 
 export default function Title(props) {
 	return (
-		<Typography variant="h5" color="primary" gutterBottom>
+		<Typography className={props.className} variant="h5" color="primary" gutterBottom>
 			{props.children}
 		</Typography>
 	);
