9240f9636e9afce42ad782367d1b01e2f16d5b79
Claudemir Todo Bom <claudemir@todobom.com>
2024-09-14 20:24:03 -0300

    allow connections with same name on different companies - fix #142

--- backend/src/services/WhatsappService/CreateWhatsAppService.ts
+++ backend/src/services/WhatsappService/CreateWhatsAppService.ts
@@ -82,7 +82,7 @@ const CreateWhatsAppService = async ({
         async value => {
           if (!value) return false;
           const nameExists = await Whatsapp.findOne({
-            where: { name: value, channel }
+            where: { name: value, companyId }
           });
           return !nameExists;
         }
