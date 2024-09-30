46fd27d6a3634b0836cdf8613ebb42ee5254f0a2
Claudemir Todo Bom <claudemir@todobom.com>
2024-09-30 18:26:30 -0300

    ShowTicketService.ts: add function to reload ticket with related objects

--- backend/src/services/TicketServices/ShowTicketService.ts
+++ backend/src/services/TicketServices/ShowTicketService.ts
@@ -6,6 +6,49 @@ import Queue from "../../models/Queue";
 import Tag from "../../models/Tag";
 import Whatsapp from "../../models/Whatsapp";
 
+const include = [
+  {
+    model: Contact,
+    as: "contact",
+    attributes: [
+      "id",
+      "name",
+      "number",
+      "email",
+      "profilePicUrl",
+      "presence",
+      "disableBot"
+    ],
+    include: ["extraInfo"]
+  },
+  {
+    model: User,
+    as: "user",
+    attributes: ["id", "name"]
+  },
+  {
+    model: Queue,
+    as: "queue",
+    attributes: ["id", "name", "color"]
+  },
+  {
+    model: Whatsapp,
+    as: "whatsapp",
+    attributes: ["name", "facebookUserToken", "facebookUserId"]
+  },
+  {
+    model: Tag,
+    as: "tags",
+    attributes: ["id", "name", "color"]
+  }
+];
+
+export const reloadTicketService = async ticket => {
+  return ticket.reload({
+    include
+  });
+};
+
 const ShowTicketService = async (
   id: string | number,
   companyId: number
@@ -14,42 +57,7 @@ const ShowTicketService = async (
     where: {
       id
     },
-    include: [
-      {
-        model: Contact,
-        as: "contact",
-        attributes: [
-          "id",
-          "name",
-          "number",
-          "email",
-          "profilePicUrl",
-          "presence",
-          "disableBot"
-        ],
-        include: ["extraInfo"]
-      },
-      {
-        model: User,
-        as: "user",
-        attributes: ["id", "name"]
-      },
-      {
-        model: Queue,
-        as: "queue",
-        attributes: ["id", "name", "color"]
-      },
-      {
-        model: Whatsapp,
-        as: "whatsapp",
-        attributes: ["name", "facebookUserToken", "facebookUserId"]
-      },
-      {
-        model: Tag,
-        as: "tags",
-        attributes: ["id", "name", "color"]
-      }
-    ]
+    include
   });
 
   if (ticket?.companyId !== companyId) {
