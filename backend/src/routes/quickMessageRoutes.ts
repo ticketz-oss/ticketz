580cfba6a2ead148aa98d58a2a70c50ef988880c
Claudemir Todo Bom <claudemir@todobom.com>
2024-10-06 11:07:36 -0300

    attach media files on QuickMessages

--- backend/src/routes/quickMessageRoutes.ts
+++ backend/src/routes/quickMessageRoutes.ts
@@ -1,7 +1,11 @@
+import multer from "multer";
 import express from "express";
 import isAuth from "../middleware/isAuth";
+import uploadConfig from "../config/upload";
 
 import * as QuickMessageController from "../controllers/QuickMessageController";
+
+const upload = multer(uploadConfig);
 
 const routes = express.Router();
 
@@ -11,9 +15,19 @@ routes.get("/quick-messages", isAuth, QuickMessageController.index);
 
 routes.get("/quick-messages/:id", isAuth, QuickMessageController.show);
 
-routes.post("/quick-messages", isAuth, QuickMessageController.store);
+routes.post(
+  "/quick-messages",
+  isAuth,
+  upload.array("file"),
+  QuickMessageController.store
+);
 
-routes.put("/quick-messages/:id", isAuth, QuickMessageController.update);
+routes.put(
+  "/quick-messages/:id",
+  isAuth,
+  upload.array("file"),
+  QuickMessageController.update
+);
 
 routes.delete("/quick-messages/:id", isAuth, QuickMessageController.remove);
 
