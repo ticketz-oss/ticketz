53ea11e9cb57c633d2b824d15eccc0045718cf07
Claudemir Todo Bom <claudemir@todobom.com>
2024-11-20 12:24:43 -0300

    SyncTagService: emit update event

--- backend/src/services/TagServices/SyncTagsService.ts
+++ backend/src/services/TagServices/SyncTagsService.ts
@@ -1,6 +1,8 @@
+import { getIO } from "../../libs/socket";
 import Tag from "../../models/Tag";
 import Ticket from "../../models/Ticket";
 import TicketTag from "../../models/TicketTag";
+import ShowTicketService from "../TicketServices/ShowTicketService";
 
 interface Request {
   tags: Tag[];
@@ -11,14 +13,27 @@ const SyncTags = async ({
   tags,
   ticketId
 }: Request): Promise<Ticket | null> => {
-  const ticket = await Ticket.findByPk(ticketId, { include: [Tag] });
+  const ticket = await ShowTicketService(ticketId);
 
   const tagList = tags.map(t => ({ tagId: t.id, ticketId }));
+
+  const io = getIO();
 
   await TicketTag.destroy({ where: { ticketId } });
   await TicketTag.bulkCreate(tagList);
 
-  ticket?.reload();
+  await ticket?.reload();
+
+  io.to(`company-${ticket.companyId}-${ticket.status}`)
+    .to(`company-${ticket.companyId}-notification`)
+    .to(`queue-${ticket.queueId}-${ticket.status}`)
+    .to(`queue-${ticket.queueId}-notification`)
+    .to(ticketId.toString())
+    .to(`user-${ticket?.userId}`)
+    .emit(`company-${ticket.companyId}-ticket`, {
+      action: "update",
+      ticket
+    });
 
   return ticket;
 };
