f434f77c53c9a0da86dbddd7f60b11fc587037d5
Claudemir Todo Bom <claudemir@todobom.com>
2025-01-21 19:46:52 +0000

    Mustache.ts: better code and fix regressions

--- backend/src/helpers/Mustache.ts
+++ backend/src/helpers/Mustache.ts
@@ -2,7 +2,7 @@ import Mustache from "mustache";
 import Ticket from "../models/Ticket";
 import User from "../models/User";
 
-export const greeting = (): string => {
+export const genGreeting = (): string => {
   const greetings = ["Boa madrugada", "Bom dia", "Boa tarde", "Boa noite"];
   const h = new Date().getHours();
   // eslint-disable-next-line no-bitwise
@@ -12,59 +12,36 @@ export const greeting = (): string => {
 export function formatBody(
   body: string,
   ticket?: Ticket,
-  user?: User,
+  currentUser?: User,
   customTags: [string, string] = null
 ): string {
-  if (!body || (!ticket && !user)) {
+  if (!body || (!ticket && !currentUser)) {
     return body;
   }
 
-  let ms = "";
-
-  const Hr = new Date();
-
-  const dd: string = `0${Hr.getDate()}`.slice(-2);
-  const mm: string = `0${Hr.getMonth() + 1}`.slice(-2);
-  const yy: string = Hr.getFullYear().toString();
-  const hh: number = Hr.getHours();
-  const min: string = `0${Hr.getMinutes()}`.slice(-2);
-  const ss: string = `0${Hr.getSeconds()}`.slice(-2);
-
-  if (hh >= 6) {
-    ms = "Bom dia";
-  }
-  if (hh > 11) {
-    ms = "Boa tarde";
-  }
-  if (hh > 17) {
-    ms = "Boa noite";
-  }
-  if (hh > 23 || hh < 6) {
-    ms = "Boa madrugada";
-  }
-
-  const setor = ticket?.queue?.name || "";
-
-  const usuario =
-    ticket?.userId === undefined || ticket.userId === null
-      ? ""
-      : ticket.user.name;
-
-  const protocol = `${yy}${mm}${dd}-${ticket.id}`;
-
-  const hora = `${hh}:${min}:${ss}`;
+  const name = ticket?.contact?.name || ticket?.contact?.number || "{{name}}";
+  const firstname = name.trim().split(" ")[0] || "{{firstname}}";
+  const greeting = genGreeting();
+  const queue = ticket?.queue?.name || "{{queue}}";
+  const user = currentUser?.name || ticket?.user?.name || "{{user}}";
+  const now = new Date();
+  const protocol = `${now.toLocaleDateString("en-GB").replace(/\//g, "")}-${
+    ticket.id
+  }`;
+  const hora = now.toLocaleTimeString("en-GB", { hour12: false });
 
   const view = {
-    name: ticket.contact?.name.trim() || "",
-    firstname: ticket.contact?.name.trim().split(" ")[0] || "",
-    gretting: greeting(),
-    ms,
+    name,
+    firstname,
+    greeting,
+    gretting: greeting,
+    ms: greeting,
     protocol,
     hora,
-    fila: setor,
-    usuario,
-    user: user?.name || ticket?.user?.name || "{{username}}",
-    queue: ticket?.queue?.name || "{{queue}}",
+    fila: queue,
+    usuario: user,
+    user,
+    queue,
     ticket: ticket?.id
   };
   return Mustache.render(body, view, null, customTags);
