a0468e3f05f116c03e674bfbc28b59c5ec5de62d
Claudemir Todo Bom <claudemir@todobom.com>
2025-03-04 16:47:00 -0300

    Refactor `saveMediaToFile` function to simplify parameters and path generation.
    - Remove `Ticket` and `Contact` dependencies
    - Add `ticketId` and `contactId` parameters
    - Simplify path generation logic
    - Ensure `companyId` is always provided
    - Update error handling and filename generation
    - Adjust imports accordingly

--- backend/src/helpers/saveMediaFile.ts
+++ backend/src/helpers/saveMediaFile.ts
@@ -1,11 +1,9 @@
 import { FileContents, FileStorage } from "@flystorage/file-storage";
 import { LocalStorageAdapter } from "@flystorage/local-fs";
-import Ticket from "../models/Ticket";
 import { getPublicPath } from "./GetPublicPath";
 import { logger } from "../utils/logger";
 import { S3Storage } from "./S3Storage";
 import { makeRandomId } from "./MakeRandomId";
-import Contact from "../models/Contact";
 
 export default async function saveMediaToFile(
   media: {
@@ -13,17 +11,10 @@ export default async function saveMediaToFile(
     mimetype: string;
     filename: string;
   },
-  ticket?: Ticket,
-  contact?: Contact,
-  companyId?: number
+  companyId: number,
+  ticketId?: number,
+  contactId?: number
 ): Promise<string> {
-  if (!ticket && !contact && !companyId) {
-    throw new Error("saveMediaToFile: No ticket, contact or company provided");
-  }
-
-  const contactId = ticket?.contactId || contact?.id;
-  companyId = companyId || ticket?.companyId || contact?.companyId;
-
   if (!media.filename) {
     const ext = media.mimetype.split("/")[1].split(";")[0];
     media.filename = `${new Date().getTime()}.${ext}`;
@@ -32,12 +23,14 @@ export default async function saveMediaToFile(
   const randomId = makeRandomId(10);
   let relativePath = `media/${companyId}/`;
   if (contactId) {
-    relativePath += `${contactId}/${randomId}`;
-  } else if (ticket) {
-    relativePath += `/${ticket.id}/${randomId}`;
-  } else {
-    relativePath += `/${randomId}`;
+    relativePath += `${contactId}/`;
   }
+
+  if (ticketId) {
+    relativePath += `${ticketId}/`;
+  }
+
+  relativePath += `/${randomId}`;
 
   const fileStorage = S3Storage.getInstance();
   await fileStorage.prepare();
