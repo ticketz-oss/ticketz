bbb5f225862b72b5bf6f5c5aecf4870b0ff24809
Claudemir Todo Bom <claudemir@todobom.com>
2025-03-05 20:49:41 -0300

    Refactor logger initialization to use a reusable function
    - Create `initLogger` function for logger initialization
    - Replace `logger` initialization with `initLogger`
    - Replace `loggerBaileys` initialization with `initLogger`
    - Add `subsystem` parameter to `initLogger`
    - Simplify logger configuration
    - Ensure consistent logging setup across subsystems

--- backend/src/utils/logger.ts
+++ backend/src/utils/logger.ts
@@ -11,41 +11,29 @@ function addSubsystem(inputArgs, subsystem) {
   }
 }
 
-export const logger = pino({
-  level: process.env.LOG_LEVEL ?? "info",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      levelFirst: true,
-      translateTime: true,
-      colorize: true
+export function initLogger(subsystem: string, level: string) {
+  return pino({
+    level,
+    transport: {
+      target: "pino-pretty",
+      options: {
+        levelFirst: true,
+        translateTime: true,
+        colorize: true
+      }
+    },
+    hooks: {
+      logMethod(inputArgs, method) {
+        addSubsystem(inputArgs, subsystem);
+        method.apply(this, inputArgs);
+      }
     }
-  },
-  hooks: {
-    logMethod(inputArgs, method) {
-      addSubsystem(inputArgs, "ticketz");
-      method.apply(this, inputArgs);
-    }
-  }
-});
+  });
+}
 
-export const loggerBaileys = pino({
-  timestamp: () => {
-    return `,"time":"${new Date().toJSON()}"`;
-  },
-  level: process.env.BAILEYS_LOG_LEVEL ?? "error",
-  transport: {
-    target: "pino-pretty",
-    options: {
-      levelFirst: true,
-      translateTime: true,
-      colorize: true
-    }
-  },
-  hooks: {
-    logMethod(inputArgs, method) {
-      addSubsystem(inputArgs, "baileys");
-      method.apply(this, inputArgs);
-    }
-  }
-});
+export const logger = initLogger("ticketz", process.env.LOG_LEVEL ?? "info");
+
+export const loggerBaileys = initLogger(
+  "baileys",
+  process.env.BAILEYS_LOG_LEVEL ?? "error"
+);
