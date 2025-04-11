6bc04e5560b78aff7a1d2c8b2a98180cb42230b1
Claudemir Todo Bom <claudemir@todobom.com>
2025-04-11 10:49:54 -0300

    @types/express.d.ts: prettier

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -1,15 +1,17 @@
 declare namespace Express {
   export interface Request {
-    user: { id: string; profile: string; isSuper: boolean, companyId: number };
-    companyId: number | undefined,
-    tokenData: {
-      id: string;
-      username: string;
-      profile: string;
-      super: boolean;
-      companyId: number;
-      iat: number;
-      exp: number;
-    } | undefined
+    user: { id: string; profile: string; isSuper: boolean; companyId: number };
+    companyId: number | undefined;
+    tokenData:
+      | {
+          id: string;
+          username: string;
+          profile: string;
+          super: boolean;
+          companyId: number;
+          iat: number;
+          exp: number;
+        }
+      | undefined;
   }
 }
