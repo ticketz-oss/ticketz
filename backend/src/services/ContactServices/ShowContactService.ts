293e7665b62ce88f48cac643c3c11c4823b6bbf1
Claudemir Todo Bom <claudemir@todobom.com>
2025-05-01 11:51:22 -0300

    ShowContactService: allow internal use
    - Updated `companyId` parameter to be optional in the function signature
    - Adjusted conditional check to handle optional `companyId`
    - Modified error message for unauthorized access
    - Ensured compatibility with cases where `companyId` is not provided

--- backend/src/services/ContactServices/ShowContactService.ts
+++ backend/src/services/ContactServices/ShowContactService.ts
@@ -3,12 +3,12 @@ import AppError from "../../errors/AppError";
 
 const ShowContactService = async (
   id: string | number,
-  companyId: number
+  companyId?: number
 ): Promise<Contact> => {
   const contact = await Contact.findByPk(id, { include: ["extraInfo"] });
 
-  if (contact?.companyId !== companyId) {
-    throw new AppError("Não é possível excluir registro de outra empresa");
+  if (companyId && contact?.companyId !== companyId) {
+    throw new AppError("Não é possível consultar registro de outra empresa");
   }
 
   if (!contact) {
