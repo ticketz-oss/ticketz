cd2dcd01499a72d89de34283c0b080bcafa78d5e
tutujaru <100329749+tutujaru@users.noreply.github.com>
2025-05-21 20:47:52 -0400

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
+
 import Paper from "@material-ui/core/Paper";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
@@ -8,15 +9,19 @@ import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
 import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
+
 // ICONS
 import GroupAddIcon from "@material-ui/icons/GroupAdd";
 import HourglassEmptyIcon from "@material-ui/icons/HourglassEmpty";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import TimerIcon from '@material-ui/icons/Timer';
+
 import { makeStyles } from "@material-ui/core/styles";
 import { grey, blue } from "@material-ui/core/colors";
 import { toast } from "react-toastify";
+
 import TableAttendantsStatus from "../../components/Dashboard/TableAttendantsStatus";
+
 import { isEmpty } from "lodash";
 import moment from "moment";
 import { i18n } from "../../translate/i18n";
@@ -24,10 +29,15 @@ import OnlyForSuperUser from "../../components/OnlyForSuperUser";
 import useAuth from "../../hooks/useAuth.js";
 import clsx from "clsx";
 import { loadJSON } from "../../helpers/loadJSON";
+
 import { SmallPie } from "./SmallPie";
 import { TicketCountersChart } from "./TicketCountersChart";
+
+import TicketzRegistry from "../../components/TicketzRegistry";
 import api from "../../services/api.js";
 import { SocketContext } from "../../context/Socket/SocketContext.js";
+
+const gitinfo = loadJSON('/gitinfo.json');
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -41,6 +51,39 @@ const useStyles = makeStyles((theme) => ({
     height: 240,
     overflowY: "auto",
     ...theme.scrollbarStyles,
+  },
+  pixkey: {
+    fontSize: "9pt",
+  },
+  paymentimg: {
+    maxWidth: "100%",
+    marginTop: "30px",
+  },
+  paymentpix: {
+    maxWidth: "100%",
+    maxHeight: "150px",
+    padding: "5px",
+    backgroundColor: "white",
+    borderColor: "black",
+    borderStyle: "solid",
+    borderWidth: "2px",
+  },
+  supportPaper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    overflowY: "auto",
+    backgroundColor: theme.palette.secondary.main,
+    color: theme.palette.secondary.contrastText,
+    ...theme.scrollbarStyles,
+  },
+  supportBox: {
+    backgroundColor: theme.palette.secondary.light,
+    borderRadius: "10px",
+    textAlign: "center",
+    borderColor: theme.palette.secondary.main,
+    borderWidth: "3px",
+    borderStyle: "solid",
   },
   cardAvatar: {
     fontSize: "55px",
@@ -101,10 +144,55 @@ const useStyles = makeStyles((theme) => ({
     position: "sticky",
     right: 0,
   },
+  ticketzProPaper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    overflowY: "auto",
+    backgroundColor: theme.palette.ticketzproad.main,
+    color: theme.palette.ticketzproad.contrastText,
+    ...theme.scrollbarStyles,
+  },
+  ticketzRegistryPaper: {
+    padding: theme.spacing(2),
+    display: "flex",
+    flexDirection: "column",
+    overflowY: "auto",
+    backgroundColor: theme.palette.background.main,
+    color: theme.palette.background.contrastText,
+    borderColor: theme.palette.primary.main,
+    borderWidth: "3px",
+    borderStyle: "solid",
+    marginBottom: "1em",
+    ...theme.scrollbarStyles,
+  },
+  ticketzProBox: {
+    textAlign: "center",
+    alignContent: "center"
+  },
+  ticketzProTitle: {
+    fontWeight: "bold"
+  },
+  ticketzProScreen: {
+    maxHeight: "300px",
+    maxWidth: "100%"
+  },
+  ticketzProFeatures: {
+    padding: 0,
+    listStyleType: "none"
+  },
+  ticketzProCommand: {
+    fontFamily: "monospace",
+    backgroundColor: "#00000080"
+  },
+  clickpointer: {
+    cursor: "pointer"
+  }
 }));
 
 const InfoCard = ({ title, value, icon }) => {
   const classes = useStyles();
+  
   return (
     <Grid item xs={12} sm={6} md={3}>
       <Paper
@@ -132,7 +220,7 @@ const InfoCard = ({ title, value, icon }) => {
       </Paper>
     </Grid>
   )
-};
+}
 
 const InfoRingCard = ({ title, value, graph }) => {
   const classes = useStyles();
@@ -176,6 +264,11 @@ const Dashboard = () => {
   );
   const [dateTo, setDateTo] = useState(moment().format("YYYY-MM-DD"));
   const { getCurrentUserInfo } = useAuth();
+    
+  const [supportBoxOpen, setSupportBoxOpen] = useState(false);
+  const [registered, setRegistered] = useState(false);
+  const [proInstructionsOpen, setProInstructionsOpen] = useState(false);
+  
   const [usersOnlineTotal, setUsersOnlineTotal] = useState(0);
   const [usersOfflineTotal, setUsersOfflineTotal] = useState(0);
   const [usersStatusChartData, setUsersStatusChartData] = useState([]);
@@ -183,20 +276,33 @@ const Dashboard = () => {
   const [pendingChartData, setPendingChartData] = useState([]);
   const [openedTotal, setOpenedTotal] = useState(0);
   const [openedChartData, setOpenedChartData] = useState([]);
+  
   const [ticketsData, setTicketsData] = useState({});
   const [usersData, setUsersData] = useState([]);
   const [loadingUsers, setLoadingUsers] = useState(false);
-  const socketManager = useContext(SocketContext);
 
+  const socketManager = useContext(SocketContext);
+    
+  async function showProInstructions() {
+    if (gitinfo.commitHash) {
+      setProInstructionsOpen(true);
+      return;
+    }
+    
+    window.open("https://pro.ticke.tz", "_blank");
+  }
+  
   useEffect(() => {
-    const socket = socketManager.GetSocket();
+    const socket = socketManager.GetSocket(companyId);
+    
     socket.on("userOnlineChange", updateStatus);
     socket.on("counter", updateStatus);
+
     return () => {
       socket.disconnect();
     }
   }, [socketManager]);
-
+  
   useEffect(() => {
     getCurrentUserInfo().then(
       (user) => {
@@ -209,17 +315,25 @@ const Dashboard = () => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(async () => {
+    const registry = await api.get("/ticketz/registry");
+
+    setRegistered( registry?.data?.disabled || !!(registry?.data?.whatsapp ) );
+  }, []);
+    
   useEffect(() => {
     fetchData();
   }, [period]);
-
+  
   async function handleChangePeriod(value) {
     setPeriod(value);
   }
 
   async function updateStatus() {
     const { data } = await api.get("/dashboard/status");
+    
     if (!data) return;
+
     let usersOnlineTotal = 0;
     let usersOfflineTotal = 0;
     data.usersStatusSummary.forEach((item) => {
@@ -229,6 +343,7 @@ const Dashboard = () => {
         usersOfflineTotal++;
       }
     });
+
     setUsersStatusChartData([
       {
         name: "Online",
@@ -241,8 +356,10 @@ const Dashboard = () => {
         color: "#ff0000"
       }
     ]);
+
     setUsersOnlineTotal(usersOnlineTotal);
     setUsersOfflineTotal(usersOfflineTotal);
+
     let pendingTotal = 0;
     let openedTotal = 0;
     const pendingChartData = [];
@@ -271,38 +388,45 @@ const Dashboard = () => {
     setOpenedTotal(openedTotal);
     setOpenedChartData(openedChartData);
   }
-
+  
   async function fetchData() {
     let params = {};
+    
     const days = Number(period);
+
     if (days) {
       params = {
         date_from: moment().subtract(days, "days").format("YYYY-MM-DD"),
         date_to: moment().format("YYYY-MM-DD")
       };
     }
+
     if (!days && !isEmpty(dateFrom) && moment(dateFrom).isValid()) {
       params = {
         ...params,
         date_from: moment(dateFrom).format("YYYY-MM-DD"),
       };
     }
+
     if (!days && !isEmpty(dateTo) && moment(dateTo).isValid()) {
       params = {
         ...params,
         date_to: moment(dateTo).format("YYYY-MM-DD"),
       };
     }
+
     if (Object.keys(params).length === 0) {
       toast.error(i18n.t("dashboard.filter.invalid"));
       return;
     }
+
     api.get("/dashboard/tickets", { params }).then(
       result => {
         if (result?.data) {
           setTicketsData(result.data);
         }
       });
+
     setLoadingUsers(true);
     api.get("/dashboard/users", { params }).then(
       result => {
@@ -383,13 +507,149 @@ const Dashboard = () => {
   }
 
   if (currentUser?.profile !== "admin") {
-    return null;
+    return (
+      <div>
+      </div>
+    );
   }
-
+      
   return (
     <div>
       <Container maxWidth="lg" className={classes.container}>
         <Grid container spacing={3} justifyContent="flex-start">
+
+          { !localStorage.getItem("hideAds") && <OnlyForSuperUser
+            user={currentUser}
+            yes={() => (
+              <Grid item xs={12}>
+                {!registered &&
+                  <Paper className={classes.ticketzRegistryPaper}>
+                    <TicketzRegistry onRegister={setRegistered} />
+                  </Paper>
+                }
+                <Paper className={clsx(classes.ticketzProPaper, {
+                  [classes.clickpointer]: !proInstructionsOpen,
+                })} onClick={() => showProInstructions()}>
+                  <Grid container justifyContent="flex-end">
+                    <Grid className={classes.ticketzProBox} item xs={12} md={proInstructionsOpen ? 4 : 6} sm={12}>
+                      <div>
+                        <img className={classes.ticketzProScreen} src="https://pro.ticke.tz/images/0/7/3/0/b/0730b234af7b4b0dac72d09828863bb7cb9193ea-ticketz-computador.png" />
+                      </div>
+                    </Grid>
+                    { !proInstructionsOpen &&
+                    <Grid className={classes.ticketzProBox} item xs={12} md={6} sm={12}>
+                      <Typography className={classes.ticketzProTitle} component="h3" variant="h5" gutterBottom>
+                        Ticketz PRO
+                      </Typography>
+                      <Typography component="h3" variant="h7" gutterBottom>
+                      <ul className={classes.ticketzProFeatures}>
+                        <li>Whatsapp Oficial - Instagram - Messenger e outros</li>
+                        <li>Features exclusivas</li>
+                        <li>Suporte Avançado</li>
+                        <li>Migração Facilitada</li>
+                      </ul>
+                      </Typography>
+                      <Typography component="h3" variant="h5">
+                        Assine por R$ 199/mês
+                      </Typography>
+                      <Typography component="h3" variant="h7" gutterBottom>
+                        direto dentro do sistema
+                      </Typography>
+                      { gitinfo.commitHash && 
+                      <Typography component="h3" variant="h5">
+                        Clique para instruções de Upgrade
+                      </Typography>
+                      }
+                      { !gitinfo.commitHash && 
+                      <Typography component="h3" variant="h5">
+                        Clique para visitar o site!
+                      </Typography>
+                      }
+                    </Grid>
+                    }
+                    { proInstructionsOpen &&
+                    <Grid className={classes.ticketzProBox} item xs={12} md={8} sm={12}>
+                      <Typography className={classes.ticketzProTitle} component="h3" variant="h5" gutterBottom>
+                        Instruções de Upgrade
+                      </Typography>
+                      <Typography paragraph>
+                        Se você instalou as imagens disponibilizadas pelo projeto em um
+                        servidor ou VPS utilizando as instruções facilitadas tudo o que
+                        você precisa fazer é acessar seu servidor e digitar o comando abaixo:
+                      </Typography>
+                      <Typography className={classes.ticketzProCommand} paragraph>
+                        curl -sSL update.ticke.tz | sudo bash -s pro
+                      </Typography>
+                      <Typography paragraph>
+                        Em instantes o Ticketz PRO estará instalado com todos os teus dados,
+                        agora só precisa ir até o menu de usuário, clicar em "Assinatura do
+                        Ticketz PRO" e fazer a sua assinatura.
+                      </Typography>
+                      <Typography paragraph>
+                        Se a tua instalação for diferente ou acredita que precisa
+                        de auxílio para instalar o Ticketz
+                        Pro, <a href="https://wa.me/554935670707"> entre
+                        em contato</a> que nós ajudamos!
+                      </Typography>
+                    </Grid>
+                    }
+                  </Grid>
+                </Paper>
+              </Grid>
+            )} />
+          }
+
+          { !localStorage.getItem("hideAds") && <OnlyForSuperUser
+            user={currentUser}
+            yes={() => (
+              <Grid item xs={12}>
+                <Paper className={clsx(classes.supportPaper, {
+                  [classes.clickpointer]: !supportBoxOpen,
+                })} onClick={() => setSupportBoxOpen(true)}>
+                  <Typography component="h2" variant="h6" gutterBottom>
+                    {i18n.t("ticketz.support.title")}
+                  </Typography>
+                  {supportBoxOpen &&
+                    <Grid container justifyContent="flex-end">
+                      <Grid className={classes.supportBox} item xs={12} md={4} sm={12}>
+                        <Typography component="h3" variant="h6" gutterBottom>
+                          PIX
+                        </Typography>
+                        <div>
+                          <img className={classes.paymentpix} src="/ticketzpix.png" />
+                        </div>
+                        <Typography className={classes.pixkey} component="body2" paragraph>
+                          1ab11506-9480-4303-8e1e-988e7c49ed4d
+                        </Typography>
+                      </Grid>
+                      <Grid className={classes.supportBox} item xs={12} md={4} sm={12}>
+                        <Typography component="h3" variant="h6" gutterBottom>
+                          {i18n.t("ticketz.support.mercadopagotitle")}
+                        </Typography>
+                        <Typography component="body2" paragraph>
+                          {i18n.t("ticketz.support.recurringbrl")}
+                        </Typography>
+                        <div><a href="https://www.mercadopago.com.br/subscriptions/checkout?preapproval_plan_id=2c9380848f1b8ed1018f2b011f90061f" target="_blank">
+                          <img className={classes.paymentimg} src="/mercadopago.png" />
+                        </a></div>
+                      </Grid>
+                      <Grid className={classes.supportBox} item xs={12} md={4} sm={12}>
+                        <Typography component="h3" variant="h6" gutterBottom>
+                          {i18n.t("ticketz.support.paypaltitle")}
+                        </Typography>
+                        <Typography component="body2" paragraph>
+                          {i18n.t("ticketz.support.international")}
+                        </Typography>
+                        <div><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=X6XHVCPMRQEL4" target="_blank">
+                          <img className={classes.paymentimg} src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" />
+                        </a></div>
+                      </Grid>
+                    </Grid>
+                  }
+                </Paper>
+              </Grid>
+            )} /> }
+
           {/* USUARIOS ONLINE */}
           <InfoRingCard
             title={i18n.t("dashboard.usersOnline")}
@@ -398,6 +658,7 @@ const Dashboard = () => {
               <SmallPie chartData={usersStatusChartData} />
             }
           />
+
           {/* ATENDIMENTOS PENDENTES */}
           <InfoRingCard
             title={i18n.t("dashboard.ticketsWaiting")}
@@ -406,6 +667,7 @@ const Dashboard = () => {
               <SmallPie chartData={pendingChartData} />
             }
           />
+
           {/* ATENDIMENTOS ACONTECENDO */}
           <InfoRingCard
             title={i18n.t("dashboard.ticketsOpen")}
@@ -414,32 +676,38 @@ const Dashboard = () => {
               <SmallPie chartData={openedChartData} />
             }
           />
+
           {/* FILTROS */}
           {renderFilters()}
+
           {/* ATENDIMENTOS REALIZADOS */}
           <InfoCard
             title={i18n.t("dashboard.ticketsDone")}
             value={ticketsData.ticketStatistics?.totalClosed || 0}
             icon={<CheckCircleIcon style={{ fontSize: 100 }} />}
           />
+
           {/* NOVOS CONTATOS */}
           <InfoCard
             title={i18n.t("dashboard.newContacts")}
             value={ticketsData.ticketStatistics?.newContacts || 0}
             icon={<GroupAddIcon style={{ fontSize: 100 }} />}
           />
+
           {/* T.M. DE ATENDIMENTO */}
           <InfoCard
             title={i18n.t("dashboard.avgServiceTime")}
             value={formatTime(ticketsData.ticketStatistics?.avgServiceTime)}
             icon={<TimerIcon style={{ fontSize: 100 }} />}
           />
+
           {/* T.M. DE ESPERA */}
           <InfoCard
             title={i18n.t("dashboard.avgWaitTime")}
             value={formatTime(ticketsData.ticketStatistics?.avgWaitTime)}
             icon={<HourglassEmptyIcon style={{ fontSize: 100 }} />}
           />
+
           {/* DASHBOARD ATENDIMENTOS NO PERÍODO */}
           <Grid item xs={12}>
             <Paper className={classes.fixedHeightPaper}>
@@ -450,6 +718,8 @@ const Dashboard = () => {
                />
             </Paper>
           </Grid>
+
+
           {/* USER REPORT */}
           <Grid item xs={12}>
             {usersData.userReport?.length ? (
@@ -459,6 +729,7 @@ const Dashboard = () => {
               />
             ) : null}
           </Grid>
+
         </Grid>
       </Container>
     </div>
