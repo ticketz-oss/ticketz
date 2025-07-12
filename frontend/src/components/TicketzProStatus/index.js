73c49a89ed15f1417e0d15bca3f2d2d1ce1c09cd
Claudemir Todo Bom <claudemir@todobom.com>
2025-07-12 12:44:07 -0300

    Add PIX payment instructions on dashboard
    - Add PIX QR code and copy-to-clipboard functionality.
    - Display next payment date for PIX subscriptions.
    - Introduce `checkSubscription` function for status updates.
    - Add loading state to handle subscription status updates.
    - Use `WhatsMarked` for PIX payment instructions display.
    - Update styles for PIX-related components.

--- frontend/src/components/TicketzProStatus/index.js
+++ frontend/src/components/TicketzProStatus/index.js
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Paper, Typography } from "@material-ui/core";
+import { Grid, Paper, Typography, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import useTicketzStatus from "../../hooks/useTicketzStatus";
 import useTicketzProStatus from "../../hooks/useTicketzProStatus";
 import moment from "moment/moment";
 import 'moment/locale/pt';
+import { i18n } from "../../translate/i18n";
+import WhatsMarked from "react-whatsmarked";
+import QRCode from "qrcode.react";
+import { copyToClipboard } from "../../helpers/copyToClipboard";
 
 moment.locale("pt-br");
 
@@ -30,6 +34,9 @@ const useStyles = makeStyles((theme) => ({
   ticketzProCounter: {
     fontSize: "35pt",
     fontWeight: "bold"
+  },
+  pixInstructions: {
+    margin: "auto"
   }
 
 }));
@@ -39,17 +46,21 @@ export default function TicketzProStatus(props) {
   const { ticketzStatus } = useTicketzStatus();
   const { ticketzProStatus } = useTicketzProStatus();
   const [proStatus, setProStatus] = useState(null);
+  const [loadingStatus, setLoadingStatus] = useState(false);
   const [status, setStatus] = useState(null);
 
+  async function checkSubscription(recheck) {
+    setLoadingStatus(true);
+    const ticketzPro = await ticketzProStatus({ recheck });
+    setProStatus(ticketzPro.status);
+    setLoadingStatus(false);
+
+    const ticketz = await ticketzStatus();
+    setStatus(ticketz);
+  }
+
   useEffect (() => {
-    async function fetchData() {
-      const ticketzPro = await ticketzProStatus();
-      setProStatus(ticketzPro.status);
-  
-      const ticketz = await ticketzStatus();
-      setStatus(ticketz);
-    }
-    fetchData();
+    checkSubscription();
   }, []);
   
 	return (
@@ -77,6 +88,46 @@ export default function TicketzProStatus(props) {
             </Typography>
             <div className={classes.ticketzProCounter}>{ status?.agents }</div>
           </Grid>
+          {proStatus?.subscriptionData?.pix && !loadingStatus &&
+          <Grid className={classes.ticketzProBox} item xs={12} md={12} sm={12}>
+              <Grid container spacing={2}>
+                <Grid className={classes.pix} xs={12} sm={4} md={4} item>
+                  {proStatus.subscriptionData?.next_payment_date &&
+                    <div>
+                      {i18n.t("common.dueDate")}:<br />{moment(proStatus.subscriptionData.next_payment_date).format("LL")}
+                    </div>
+                  }
+                  <QRCode value={proStatus.subscriptionData.pix}
+                    style={
+                      {
+                        borderStyle: "solid",
+                        borderWidth: "1px",
+                        padding: "5px",
+                        borderColor: "black",
+                        backgroundColor: "white",
+                        height: "auto",
+                        maxWidth: "100%"
+                      }} />
+                  <br />
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => copyToClipboard(proStatus.subscriptionData.pix)}>
+                    {i18n.t("common.copy")}
+                  </Button>
+                </Grid>
+                <Grid className={classes.pixInstructions} xs={12} sm={8} md={8} item>
+                  <WhatsMarked>
+                    {i18n.t("ticketz.pro.pixFinishInstructions")}
+                  </WhatsMarked>
+                  <div>&nbsp;</div>
+                  <Button variant="contained" onClick={() => checkSubscription(true)} color="primary">
+                    {i18n.t("common.proceed")}
+                  </Button>
+                </Grid>
+              </Grid>
+          </Grid>
+          }
         </Grid>
         <Typography component="h2" variant="h6">
           Status da assinatura:&nbsp;
