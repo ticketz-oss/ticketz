2cc61f528110c862ea845fc76a3f76100ca5db84
tutujaru <100329749+tutujaru@users.noreply.github.com>
2025-08-07 21:32:42 -0400

    Update index.js

--- frontend/src/components/AboutModal/index.js
+++ frontend/src/components/AboutModal/index.js
@@ -1,6 +1,4 @@
 import React, { useState, useEffect } from "react";
-
-
 import { makeStyles } from "@material-ui/core/styles";
 import {
   Button,
@@ -11,11 +9,9 @@ import {
   DialogContent,
   DialogTitle
 } from "@material-ui/core";
-
 import { i18n } from "../../translate/i18n";
 import useAuth from "../../hooks/useAuth.js";
 import { useTheme } from "@material-ui/core/styles";
-
 import { loadJSON } from "../../helpers/loadJSON";
 import api from "../../services/api";
 
@@ -39,24 +35,16 @@ const useStyles = makeStyles(theme => ({
     margin: "0 auto",
     content: `url("${theme.calculatedLogo()}")`
   },
-  ticketzLogoImg: {
-    width: "100%",
-    margin: "0 auto",
-    content: "url(" + (theme.mode === "light" ? logo : logoDark) + ")"
-  },
   textCenter: {
     textAlign: "center",
   }
-
 }));
 
 const AboutModal = ({ open, onClose }) => {
 	const classes = useStyles();
   const { getCurrentUserInfo } = useAuth();
   const [currentUser, setCurrentUser] = useState({});
-  const [backendGitInfo, setBackendGitInfo] = useState(null);
   const theme = useTheme();
-
 
 	const handleClose = () => {
 		onClose();
@@ -68,12 +56,6 @@ const AboutModal = ({ open, onClose }) => {
         setCurrentUser(user);
       }
     );
-
-    api.get("/").then(
-      (response) => {
-        setBackendGitInfo(response.data);
-      }
-    )
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
@@ -92,49 +74,14 @@ const AboutModal = ({ open, onClose }) => {
 				<DialogContent dividers>
 				{ currentUser?.super ? 
           <>
-            <div>
-              <img className={classes.ticketzLogoImg} />
-            </div>
-            <Typography variant="body1" gutterBottom><b>Frontend: 
-              { frontendGitInfo.tagName && `Version: ${frontendGitInfo.tagName} Build info: ${frontendGitInfo.buildTimestamp}` }
-              { !frontendGitInfo.tagName &&
-                <>
-                  {frontendGitInfo.commitHash && `Commit: {frontendGitInfo.commitHash} `}
-                  {frontendGitInfo.branchName && `Branch: {frontendGitInfo.branchName} `}
-                  {frontendGitInfo.commitTimestamp && `Time: {frontendGitInfo.commitTimestamp} `}
-                </>
-              }
-            </b>
-            {backendGitInfo &&
-            <>
-            <br /><b>Backend: 
-              { backendGitInfo.tagName && `Version: ${backendGitInfo.tagName} Build info: ${backendGitInfo.buildTimestamp}` }
-              { !backendGitInfo.tagName &&
-                <>
-                  {backendGitInfo.commitHash && `Commit: {backendGitInfo.commitHash} `}
-                  {backendGitInfo.branchName && `Branch: {backendGitInfo.branchName} `}
-                  {backendGitInfo.commitTimestamp && `Time: {backendGitInfo.commitTimestamp} `}
-                </>
-              }
-            </b>
-            </>
-            }
-            </Typography>
             <Typography variant="body1">{i18n.t("about.aboutdetail")}</Typography>
-            <Typography><Link target="_blank" href="https://todobom.com">{i18n.t("about.aboutauthorsite")}</Link></Typography>
-            <Typography><Link target="_blank" href="https://github.com/canove/whaticket-community">{i18n.t("about.aboutwhaticketsite")}</Link></Typography>
-            <Typography><Link target="_blank" href="https://github.com/vemfazer">{i18n.t("about.aboutvemfazersite")}</Link></Typography>
-            <Typography variant="h4">{i18n.t("about.licenseheading")}</Typography>
-            <Typography variant="body1">{i18n.t("about.licensedetail")}</Typography>
-            <Typography><Link target="_blank" href="https://github.com/ticketz-oss/ticketz/blob/main/LICENSE.md">{i18n.t("about.licensefulltext")}</Link></Typography>
-            <Typography><Link target="_blank" href="https://github.com/ticketz-oss/ticketz">{i18n.t("about.licensesourcecode")}</Link></Typography>
           </>
           :
           <>
             <div>
-              <img className={classes.logoImg} />
+              <img className={classes.logoImg} alt="Logo" />
             </div>
-            <Typography className={classes.textCenter} ><Link target="_blank" href="https://ticke.tz">{i18n.t("about.copyright")}</Link></Typography>
+            <Typography className={classes.textCenter} ><Link target="_blank" href="https://github.com/tutujaru">{i18n.t("about.copyright")}</Link></Typography>
           </>
         }
 				</DialogContent>
