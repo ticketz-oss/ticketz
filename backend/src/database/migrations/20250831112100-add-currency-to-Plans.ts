81d504fef8b1931c2594923ba817db9b3631fbae
Claudemir Todo Bom <claudemir@todobom.com>
2025-09-01 09:45:44 -0300

    Simplify migration of Plans currency field
    - Remove manual transaction management in migration
    - Use default transaction behavior of Sequelize

--- backend/src/database/migrations/20250831112100-add-currency-to-Plans.ts
+++ backend/src/database/migrations/20250831112100-add-currency-to-Plans.ts
@@ -1,40 +1,14 @@
-import { QueryInterface, DataTypes, Transaction } from "sequelize";
+import { QueryInterface, DataTypes } from "sequelize";
 
 export default {
   up: async (queryInterface: QueryInterface) => {
-    const transaction: Transaction =
-      await queryInterface.sequelize.transaction();
-
-    try {
-      await queryInterface.addColumn(
-        "Plans",
-        "currency",
-        {
-          type: DataTypes.STRING,
-          allowNull: true
-        },
-        { transaction }
-      );
-
-      await transaction.commit();
-    } catch (error) {
-      await transaction.rollback();
-      throw error;
-    }
+    return queryInterface.addColumn("Plans", "currency", {
+      type: DataTypes.STRING,
+      allowNull: true
+    });
   },
 
   down: async (queryInterface: QueryInterface) => {
-    const transaction: Transaction =
-      await queryInterface.sequelize.transaction();
-
-    try {
-      await queryInterface.removeColumn("Plans", "currency", {
-        transaction
-      });
-      await transaction.commit();
-    } catch (error) {
-      await transaction.rollback();
-      throw error;
-    }
+    return queryInterface.removeColumn("Plans", "currency");
   }
 };
