6488b734067bfc88f4ced11d349ccb5b71dda04b
allgood <claudemir@todobom.com>
2025-11-13 15:28:52 -0300

    Check for Brazilian whatsapp number duplicity
    - Import `Op` and `WhereOptions` from sequelize
    - Build dynamic where clause for number and companyId

--- backend/src/services/ContactServices/CreateContactService.ts
+++ backend/src/services/ContactServices/CreateContactService.ts
@@ -1,3 +1,4 @@
+import { Op, WhereOptions } from "sequelize";
 import AppError from "../../errors/AppError";
 import Contact from "../../models/Contact";
 import ContactCustomField from "../../models/ContactCustomField";
@@ -27,8 +28,22 @@ const CreateContactService = async ({
   disableBot = false,
   language
 }: Request): Promise<Contact> => {
+  const where: WhereOptions = { number, companyId };
+
+  if (number.startsWith("55") && number.length === 13 && number[4] === "9") {
+    const brEightDigitsNumber = `${number.slice(0, 4)}${number.slice(5)}`;
+    where.number = {
+      [Op.or]: [number, brEightDigitsNumber]
+    };
+  } else if (number.startsWith("55") && number.length === 12) {
+    const brNineDigitsNumber = `${number.slice(0, 4)}9${number.slice(4)}`;
+    where.number = {
+      [Op.or]: [number, brNineDigitsNumber]
+    };
+  }
+
   const numberExists = await Contact.findOne({
-    where: { number, companyId }
+    where
   });
 
   if (numberExists) {
