4aadb621614792b08c92ac9e78bf4c9651c65e32
allgood <claudemir@todobom.com>
2025-11-20 09:44:31 -0300

    ShowUserService.ts: prettier

--- backend/src/services/UserServices/ShowUserService.ts
+++ backend/src/services/UserServices/ShowUserService.ts
@@ -3,8 +3,10 @@ import AppError from "../../errors/AppError";
 import Queue from "../../models/Queue";
 import Company from "../../models/Company";
 
-const ShowUserService = async (id: string | number, requestUserId: string | number = null): Promise<User> => {
-
+const ShowUserService = async (
+  id: string | number,
+  requestUserId: string | number = null
+): Promise<User> => {
   const requestUser = requestUserId ? await User.findByPk(requestUserId) : null;
   const user = await User.findByPk(id, {
     attributes: [
@@ -26,7 +28,11 @@ const ShowUserService = async (id: string | number, requestUserId: string | numb
     throw new AppError("ERR_NO_USER_FOUND", 404);
   }
 
-  if (requestUser && requestUser.super === false && user.companyId !== requestUser.companyId) {
+  if (
+    requestUser &&
+    requestUser.super === false &&
+    user.companyId !== requestUser.companyId
+  ) {
     throw new AppError("ERR_FORBIDDEN", 403);
   }
 
