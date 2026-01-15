99ac4d94cd3048e4c697f16beae12bcffdf2ba7f
tutujaru <100329749+tutujaru@users.noreply.github.com>
2026-01-15 17:39:33 -0400

    Add optional ticket property to Express Request

--- backend/src/@types/express.d.ts
+++ backend/src/@types/express.d.ts
@@ -2,5 +2,6 @@ declare namespace Express {
   export interface Request {
     user: { id: string; profile: string; isSuper: boolean; companyId: number };
     companyId: number | undefined;
+    ticket?: Ticket;
   }
 }
